4ec940d43ccfb8b020ac292c95d46e436b7f6577
VICTOR HUGO CAGNIN ALVES <victor.hcalves2@senacsp.edu.br>
2019-08-08 20:37:53 -0300

    Conhecendo regras

---
+++ Regras.js
@@ -0,0 +1,32 @@
+// Exemplo para não se fazer 
+// let 1nome = '';
+
+// Exemplo para se fazer 
+// string
+let nome = 'vinicius'
+
+// tipo inteiro
+let idade = 20
+
+// Tipo Decimal
+let peso = 55.25
+
+//Tipo Boleano 
+let visualizado = true;
+
+// Criar constante
+const nome1 = 'vinicius';
+
+// Exibir informações 
+console.log('Este é o console.log')
+
+console.log('Exibindo variáveis');
+console.log('Nome: ' + nome);
+nome = 'José';
+console.log('Nome: ' + nome);
+nome2 = 'Roberto';
+console.log('Nome const: ' + nome2);
+
+console.log('Nome' + nome + ' idade: '+ idade);
+console.log(`Nome: ${nome} idade ${idade}`)
+
