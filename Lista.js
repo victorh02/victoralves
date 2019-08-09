7e149162bcbf7f7c2a59c0f6fe35e82cc92b6763
VICTOR HUGO CAGNIN ALVES <victor.hcalves2@senacsp.edu.br>
2019-08-08 22:09:59 -0300

    Treinamento github

---
+++ Lista.js
@@ -0,0 +1,21 @@
+// Antigamente 
+var listaAntiga = Array();
+
+// Novo 
+let lista = [];
+console.log(lista);
+
+// Adicionando um item
+lista.push('carvão');
+lista.push('carne')
+lista.push('cerveja sem alcool')
+lista.push(10)
+console.log(lista);
+
+
+// Todo array começa com zero
+console.log(lista[0]);
+
+// O pop sempre remove o ultimo item da lista
+console.log(lista.pop())
+console.log(lista);
